d05d2ffe1ff89c19f7128a0298279db53af6f36f
Dilippm <dilippm92@gmail.com>
2024-10-28 16:52:33 +0530

    final deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000", {
+			const socket = io("https://chat-app-4qk8.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
